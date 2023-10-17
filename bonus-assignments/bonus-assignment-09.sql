528dcc615dea114d5f39f9f7f58bd12a7c3e8aff
Adam Spannbauer <SpannbauerAdam@gmail.com>
2023-10-17 14:18:18 -0400

    add bonus 09

---
+++ bonus-assignments/bonus-assignment-09.sql
@@ -0,0 +1,36 @@
+-- Use your own MySQL database at setupmysql.utk.edu
+--
+-- Consider this data about a very cheery subject: layoffs
+-- More on data: https://layoffdata.com/data/
+--
+-- Download CSV from here:
+-- https://docs.google.com/spreadsheets/d/1s2FUd66fdhvKpC_tcS_CkSjYywixLl3Fa04OU8gCekQ/edit#gid=0
+-- File > Download > Comma Separated Values (.csv)
+--
+-- 1. Create a table to hold this data in your database
+
+-- 2. Load the data into the newly created table and report the number of rows
+
+-- 3. Report on any cleaning you did outside of this file to make 2 work
+--    If any cleaning is needed you can choose easy way out (ie remove problem rows)
+--    If no cleaning needed leave blank and move on.
+
+-- 4. Insert a new record into your table
+--    use "BLAHBLAH" as the value for the state field;
+--    make up the rest or reuse BLAH etc
+
+-- 5. Write a SELECT query to prove your row is there
+
+-- 6. Delete your new record from your TABLE
+
+-- 7. Repeat your query from 5 to prove your row isn't there
+
+-- 8. This is real data.  Do a little exploration.  Provide 1
+--    SELECT query that provides some interesting tidbit.
+--    The tidbit doesn't have to be mindblowing. If you're
+--    stuck, start with grouping by something, aggregating something,
+--    and sorting
+
+-- 9. Provide a caption for your query/results from 8
+
+
